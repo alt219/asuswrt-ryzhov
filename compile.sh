9f862471fa080010f7c62285dc665935a9a02ba5
Alexander Ryzhov <ryzhovau@gmail.com>
2012-10-21 01:04:38 +0400

    bump to 3.0.0.4.246 version

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 
 echo Downloading and unpacking firmware sources...
 HOME_DIR=$(pwd -P)
-FW_ZIP=GPL_RT_N66U_B1_3004220.zip
-FW_TGZ=GPL_RT-N66U_3.0.0.4.220.tgz
+FW_ZIP=GPL_RT_N66U_B1_3004246.zip
+FW_TGZ=GPL_RT-N66U_3.0.0.4.246.tgz
 cd ..
 [ -e "./$FW_ZIP" ] || wget http://dlcdnet.asus.com/pub/ASUS/wireless/RT-N66U_B1/$FW_ZIP
 [ -e "./$FW_TGZ" ] || unzip ./$FW_ZIP
@@ -11,12 +11,14 @@ cd ..
 tar -xzf ./$FW_TGZ
 cd $HOME_DIR
 
-echo Fixing broken 3.0.0.4.220 build tree...
+echo Fixing broken 3.0.0.4.246 build tree...
 mkdir ../asuswrt/release/src/router/wps/prebuilt/
-cp -f ./prebuilded/wps_monitor ../asuswrt/release/src/router/wps/prebuilt/
+cp -f ./missing_files/wps_monitor ../asuswrt/release/src/router/wps/prebuilt/
+mkdir ../asuswrt/release/src/router/asuswebstorage/prebuilt
+cp -f ./missing_files/asuswebstorage ../asuswrt/release/src/router/asuswebstorage/prebuilt
 patch -p0 -i ./patches/lighttpd.patch
-mkdir ../asuswrt/release/src-rt/router/asuswebstorage/prebuilt
-cp -f ./prebuilded/asuswebstorage ../asuswrt/release/src-rt/router/asuswebstorage/prebuilt
+cp -f ./missing_files/Makefile.am.gpl ../asuswrt/release/src/router/lighttpd-1.4.29/src/
+cp -f ./missing_files/mod_sharelink.so.l ../asuswrt/release/src/router/lighttpd-1.4.29/prebuild/
 
 
 echo Adding JFFS, SSH and custom scripts support...
@@ -34,8 +36,13 @@ sudo ln -s $(cd .. && pwd -P)/asuswrt /media/ASUSWRT/asuswrt
 
 echo Compiling firmware. Now its up to ASUS guys...
 cd ../asuswrt/release/src-rt/
-#make rt-n16
+
+# For RT-N66U
 make rt-n66u
 
+# For RT-N16
+#make rt-n16
+
+# For AC-66U
 #cd ../asuswrt/release/src-rt-6.x/
 #make rt-ac66u
